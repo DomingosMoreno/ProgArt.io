9d472cf135441f2c22d17515d28e97d3b8b9ee63
DomingosMoreno <domingosmoreno2@gmail.com>
2021-04-01 16:33:15 -0100

    v1.0
    Primeira verão do site

---
+++ scrips/main.js
@@ -0,0 +1,22 @@
+//sem projetos ainda
+
+function projetos(){
+    alert("Aviso! Ainda não há projetos importantes para mostrar.");
+}
+//sem arquivos disponíveis
+function vazio(){
+    alert("Infelizmente ainda não terminei esse arquivo :)");
+}
+
+//parte do site que o usuário se encontra 
+function place()
+{
+    
+    var ativo = document.getElementById("ativo");
+    ativo.style.backgroundColor = "red";
+    
+}
+
+
+
+
